049d71d47b892e3a0dbeb589cf357b7e162c1771
bishot <andres-120397@hotmail.com>
2020-01-30 10:09:05 -0500

    Componentes y fake api

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className='carousel-item'>
-    <img className='carousel-item__img' src='../assets/static/img1.jpg' alt='People' />
+    <img className='carousel-item__img' src={cover} alt={title} />
     <div className='carousel-item__details'>
       <div>
         <img src={playIcon} alt='Play Item' />
         <img src={plusIcon} alt='Plus Item' />
       </div>
-      <p className='carousel-item__details--title'>Titulo</p>
-      <p className='carousel-item__details--subtitle'>Subtitulo asdasd asdasdasd asdasd asdasdasd</p>
+      <p className='carousel-item__details--title'>{title}</p>
+      <p className='carousel-item__details--subtitle'>
+        {`${year} ${contentRating} ${duration}`}
+      </p>
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+  const initialState = useInitialState(API);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initialState.mylist && initialState.mylist.lenght > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Categorias'>
+        <Carousel>
+          {initialState.trends && initialState.trends.map((item) => {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            return (<CarouselItem key={item.id} {...item} />);
+          })}
+        </Carousel>
+      </Categories>
+      <Categories title='Mi Lista 2'>
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
